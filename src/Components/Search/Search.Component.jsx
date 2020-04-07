74244b4be196c8eeb4b2ad790ad411c0fa8e6e01
Sewa KPOTY <sewa@lsoffice.local>
2020-04-07 02:08:06 -0400

    Created my first react app

---
+++ src/Components/Search/Search.Component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Search.Style.css'
+
+export const Search = ({placeholder, handleChange}) => {
+    return (
+        <input className='search' type='search' placeholder={placeholder}
+                onChange={ handleChange }
+                />
+    )
+}
