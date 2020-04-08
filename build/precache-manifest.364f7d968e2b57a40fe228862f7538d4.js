2731d6949898f23cc0661905cb7ca1c27557a126
jacqueskpoty <jacques.kpoty@gmail.com>
2020-04-07 20:12:39 -0400

    first react app
    first creact app

---
+++ build/precache-manifest.364f7d968e2b57a40fe228862f7538d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a4df44e9b8b86854f9844a541832a7",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "a25d96b56376ad826f60",
+    "url": "/monsters-rolodex-react/static/css/main.c0272fb5.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex-react/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25d96b56376ad826f60",
+    "url": "/monsters-rolodex-react/static/js/main.8197b7f0.chunk.js"
+  },
+  {
+    "revision": "18908f6eb88b51269a6a",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.b428d943.js"
+  }
+]);
